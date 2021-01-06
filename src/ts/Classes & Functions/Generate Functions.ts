726d15458ca2dfa73f0f3c20209295ac936b7b4c
Deadreyo <ahmedatwa866@yahoo.com>
2021-01-06 19:35:04 +0200

    Update Generate Functions.ts

--- src/ts/Classes & Functions/Generate Functions.ts
+++ src/ts/Classes & Functions/Generate Functions.ts
@@ -14,29 +14,21 @@ export function TemplateReplace(kind: number) {try{
                 text = JASS.declares
             }
         } else {
-            alert('type is'+el.type)
             eval(`text = JASS.${el.type}`)
-            alert(`text = JASS.${el.type}`)
         }
-        alert('step 2.1')
         let textEdit = text.replace(/FRvar/gi, el.name )
-        alert('step 2.2')
         if(kind == 0) {
             sumText += textEdit;
             continue;
         }
-        alert('step 3')
         
         if(el.parentIndex == 0) textEdit = textEdit.replace("OWNERvar", "BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0)");
-        alert('step 4')
         textEdit = textEdit.replace("TOPLEFTXvar", `${((el.element.offsetLeft - coordsIMG.getBoundingClientRect().x)/coordsIMG.offsetWidth * 0.8).toPrecision(6)}`)
         textEdit = textEdit.replace("TOPLEFTYvar", `${((coordsIMG.getBoundingClientRect().bottom - el.element.getBoundingClientRect().top)/coordsIMG.height * 0.6).toPrecision(6)}`)
-        alert('step 5')
         textEdit = textEdit.replace("BOTRIGHTXvar", `${((el.element.offsetLeft - coordsIMG.getBoundingClientRect().x + el.element.width)/coordsIMG.offsetWidth * 0.8).toPrecision(6)}`)
         textEdit = textEdit.replace("BOTRIGHTYvar", `${((coordsIMG.getBoundingClientRect().bottom - el.element.getBoundingClientRect().bottom)/coordsIMG.height * 0.6).toPrecision(6)}`)
         textEdit = textEdit.replace("PATHvar", '"'+el.texturePath+'"')
         sumText += textEdit;
-        alert('step 6')
     }
     return sumText;
 }catch(e){alert(e)}} 
