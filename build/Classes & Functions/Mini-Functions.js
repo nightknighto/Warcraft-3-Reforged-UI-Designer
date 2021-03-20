0782c16bf67a3c1c93f9658fcdad4be048f9a04e
Dominik Ričko <73482103+DominikRicko@users.noreply.github.com>
2021-03-20 21:15:28 +0100

    Removing build directory
    It was not supposed to be on GitHub

--- build/Classes & Functions/Mini-Functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.debug = void 0;
-function debug(stuff) {
-    document.getElementById("debug").innerText = stuff;
-}
-exports.debug = debug;
-//# sourceMappingURL=Mini-Functions.js.map
