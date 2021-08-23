0b8b66b65692570542af78849385c9a5e59792ef
Deadreyo <ahmedatwa866@yahoo.com>
2021-08-23 23:14:01 +0200

    DebugText now refreshes Element Panel

--- src/ts/Classes & Functions/Mini-Functions.ts
+++ src/ts/Classes & Functions/Mini-Functions.ts
@@ -1,8 +1,14 @@
 import { Editor } from "../Editor/Editor";
+import { ParameterEditor } from "../Editor/ParameterEditor";
+import { ProjectTree } from "../Editor/ProjectTree";
 
 export function debugText(stuff: string) : void{
 
     Editor.GetDocumentEditor().debugLine.innerText = stuff;
+
+    if(ProjectTree.getSelected()) { //update Element Panel
+        ParameterEditor.inst().updateFields(ProjectTree.getSelected())
+    }
 }
 
 /**This will divide the number by 1000, then set precision to 4 */
