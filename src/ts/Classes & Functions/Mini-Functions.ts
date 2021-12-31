e970cff8d8bac763f4fbfc3e7c3c57cf2781d0f3
NightKnight <Ahmedatwa866@yahoo.com>
2021-12-31 23:48:23 +0200

    position precision increased to 5 from 4

--- src/ts/Classes & Functions/Mini-Functions.ts
+++ src/ts/Classes & Functions/Mini-Functions.ts
@@ -13,5 +13,5 @@ export function debugText(stuff: string) : void{
 
 /**This will divide the number by 1000, then set precision to 4 */
 export function InputEdit(value: number) : string {
-    return (value /1000).toPrecision(4)
+    return (value /1000).toPrecision(5)
 }
